b7e5e0795c5aaacb735a5f2a2f44e36876a04bb8
NessySweet <va.ogbuehi@gmail.com>
2015-02-14 13:58:28 -0500

    Add mouse-down and mouse-up functions in JS

--- js/app.js
+++ js/app.js
@@ -5,9 +5,24 @@ $(document).ready(function () {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
-	
+
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+	})
+
+	.mousedown(function() {
+		//play hadouken sound
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		//animate it to screen right
+	})
+
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+
+		//ryu goes back to ready position
 	})
 });
